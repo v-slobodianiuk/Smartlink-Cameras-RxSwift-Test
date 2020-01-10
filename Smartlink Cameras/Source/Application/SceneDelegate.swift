4d552f326a775f6a1bd3b23c16c574e788b72f66
Minhaz Mohammad <minhaz.mohammad@securenettech.com>
2020-01-10 11:25:25 -0500

    Saving progress

--- Smartlink Cameras/Source/Application/SceneDelegate.swift
+++ Smartlink Cameras/Source/Application/SceneDelegate.swift
@@ -6,11 +6,19 @@
 //  Copyright © 2020 SecureNet Technologies, LLC. All rights reserved.
 //
 
+import RxSwift
 import UIKit
-import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
+    // MARK: - Logic variables
+    fileprivate lazy var appCoordinator: AppCoordinator = {
+        let coordinator = AppCoordinator(window: window!)
+        return coordinator
+    }()
+    
+    fileprivate let disposeBag = DisposeBag()
+    
     var window: UIWindow?
 
 
@@ -20,18 +28,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Get the managed object context from the shared persistent container.
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-
-        // Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
-        // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
-        let contentView = ContentView().environment(\.managedObjectContext, context)
+//        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
-            self.window = window
-            window.makeKeyAndVisible()
+            window = UIWindow(windowScene: windowScene)
+            appCoordinator.start()
+                .subscribe()
+                .disposed(by: disposeBag)
         }
     }
 
--- Smartlink Cameras/Source/Main/MainTabBarController.swift
+++ Smartlink Cameras/Source/Main/MainTabBarController.swift
@@ -8,13 +8,85 @@
 
 import UIKit
 
+enum ImageType {
+    
+}
+
+extension ImageType {
+    
+    static func setImage(name: String) -> UIImage {
+        guard let image = UIImage(named: name) else {
+            fatalError("Specific UIImage could not be found from assets: \(name)")
+        }
+        return image
+    }
+    
+}
+
+enum TabBarItems: Int, CaseIterable {
+    case activity
+    case cameraList
+    case settings
+    
+    var image: UIImage {
+        switch self {
+            case .activity:
+                return ImageType.setImage(name: "icon-activity")
+            
+            case .cameraList:
+                return ImageType.setImage(name: "icon-video")
+            
+            case .settings:
+                return ImageType.setImage(name: "icon-settings")
+        }
+    }
+    
+    static func allCases() -> [TabBarItems] {
+        return TabBarItems.allCases
+    }
+}
+
 final class MainTabBarController: UITabBarController {
 
+    // MARK: - UI variables
+    fileprivate lazy var tabBarAppearance: UITabBarAppearance = {
+        let appearance = UITabBarAppearance()
+        appearance.backgroundImage = UIImage()
+        appearance.shadowColor = .clear
+        appearance.shadowImage = UIImage()
+        return appearance
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        view.backgroundColor = .yellow
+        configureAppearance()
     }
 
+}
+
+extension MainTabBarController {
+    
+    fileprivate func configureAppearance() {
+        if #available(iOS 13.0, *) {
+            tabBar.standardAppearance = tabBarAppearance
+        }
+        else {
+            tabBar.backgroundImage = UIImage()
+            tabBar.isTranslucent = true
+            tabBar.itemPositioning = .centered
+            tabBar.shadowImage = UIImage()
+        }
+        
+        viewControllers = TabBarItems.allCases().map({ item -> UINavigationController in
+            let newNC = UINavigationController()
+            newNC.tabBarItem = UITabBarItem(title: nil,
+                                            image: item.image,
+                                            tag: item.hashValue)
+            return newNC
+        })
+    }
+    
 }
